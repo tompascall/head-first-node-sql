aef3e5707c30d5de6f47d8f0081a42de614c5569
Gulacsy-Toth <tompascall@gmail.com>
2018-10-21 22:28:53 +0200

    add more input fields

--- src/views/aliens/abduction-form.jsx
+++ src/views/aliens/abduction-form.jsx
@@ -1,19 +1,49 @@
 const React = require('react');
 const DefaultLayout = require('../layouts/default');
 
+const style = `
+  .aliens-form label {
+    margin-right: 10px;
+  }
+
+  .aliens-form input {
+    margin-bottom: 10px;
+  }
+`;
+
 class AbductionForm extends React.Component {
   render() {
     return (
       <DefaultLayout title={this.props.title}>
-        <h1>Aliens Abducted Me -  Report an Abduction</h1>
-        <p>Share your story of alien abduction</p>
-        <form method='post' action='/test-post'>
-          <label htmlFor='firstname'>First Name:</label>
-          <input name='firstname' defaultValue='tom'/><br/>
-          <label htmlFor='lastname'>Last Name:</label>
-          <input name='lastname' defaultValue='pascall'/><br/>
-          <button type='submit' name='submit'>Send</button>
-        </form>
+        <div className='aliens-form'>
+          <style>{ style }</style>
+          <h1>Aliens Abducted Me -  Report an Abduction</h1>
+          <p>Share your story of alien abduction</p>
+          <form method='post' action='/test-post'>
+            <label htmlFor='firstname'>First Name:</label>
+            <input name='firstname' defaultValue='tom'/><br/>
+            <label htmlFor='lastname'>Last Name:</label>
+            <input name='lastname' defaultValue='pascall'/><br/>
+            <label htmlFor='email '>Email address:</label>
+            <input name='email' type='email' defaultValue='tompascall@gmail.com'/><br/>
+            <label htmlFor='whenithappened'>When did it happen?</label>
+            <input name='whenithappened' type='date' defaultValue='2011-12-23'/><br/>
+            <label htmlFor='howlong'>How long were you gone?</label>
+            <input name='howlong' defaultValue='2 days'/><br/>
+            <label htmlFor='howmany'>How many did you see?</label>
+            <input name='howmany' type='number' defaultValue='5'/><br/>
+            <label htmlFor='aliendescription'>Describe them:</label>
+            <input name='aliendescription' defaultValue='tompascall@gmail.com'/><br/>
+            <label htmlFor='whattheydid'>What did they do</label>
+            <input name='whattheydid' defaultValue='Enlightened me'/><br/>
+            <label htmlFor='fangspotted'>Have you seen my dog Fang?</label><br/>
+            Yes <input name='fangspotted' type='radio' defaultValue='yes'/>
+            No <input name='fangspotted' type='radio' defaultValue='no'/><br/>
+            <label htmlFor='other'>Anything else you want to add:</label>
+            <textarea name='other' type='textarea' defaultValue=''/><br/>
+            <button type='submit' name='submit'>Send</button>
+          </form>
+        </div>
       </DefaultLayout>
     );
   }
--- src/views/layouts/default.jsx
+++ src/views/layouts/default.jsx
@@ -1,10 +1,18 @@
 const React = require('react');
 
+const globalStyle = `
+  body {
+    font-family: monospace;
+  }
+`;
+
 class DefaultLayout extends React.Component {
   render() {
     return (
       <html>
-        <head><title>{this.props.title}</title></head>
+        <head><title>{this.props.title}</title>
+          <style>{ globalStyle }</style>
+        </head>
         <body>{this.props.children}</body>
       </html>
     );
